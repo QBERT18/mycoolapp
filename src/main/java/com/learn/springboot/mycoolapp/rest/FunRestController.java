d73c60de69a4dae68aff3b20b2c415458c19b216
Q-bert <valeh.mustafayev.98@gmail.com>
2026-02-28 21:55:19 +0100

    read data from application.properties file

--- src/main/java/com/learn/springboot/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/learn/springboot/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,19 @@
 package com.learn.springboot.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // read data from application.properties file
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello() {
