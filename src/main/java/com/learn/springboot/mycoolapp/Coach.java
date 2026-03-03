b9a5adcc45c52fffbb72b2aeb58c84dde21e5d98
Q-bert <valeh.mustafayev.98@gmail.com>
2026-03-03 21:58:01 +0100

    constructor injection

---
+++ src/main/java/com/learn/springboot/mycoolapp/Coach.java
@@ -0,0 +1,6 @@
+package com.learn.springboot.mycoolapp;
+
+public interface Coach {
+
+    String getDailyWorkout();
+}
---
+++ src/main/java/com/learn/springboot/mycoolapp/rest/DemoController.java
@@ -0,0 +1,22 @@
+package com.learn.springboot.mycoolapp.rest;
+
+import com.learn.springboot.mycoolapp.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(Coach newCoach) {
+        this.myCoach = newCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
